ee80be3a48987731cdfb34b4c2ac802ca138538a
Noor Abid <your.email@example.com>
2025-09-11 11:17:44 +1000

    fix: adjust community page CSS styling

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import CommunityPage from './pages/CommunityPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App /> 
-    {/* <CommunityPage/> */}
+    {/* <App />  */}
+    <CommunityPage/>
   </StrictMode>,
 )
